2f0a66e78aa2d889b39aa81c6e83aa5c283d5972
Jonas Rinke <j.rinke@htp-tel.de>
2020-08-27 14:34:05 +0200

    Added code to patch the item names and menu text

--- patch.py
+++ patch.py
@@ -76,6 +76,93 @@ def parse_text(text):
 
 #===========================================
 
+item_names = [
+	"PONARDO",			#hand dagger
+	"LONGA GLAVO",		#long sword
+	"TRIOBLA KLINGO",	#giant blade
+	"DRAKMORTIGANTO",	#dragon slayer
+	"LEDA ARMAĴO",		#leather armor
+	"GARNITA VESTO",	#studded mail
+	"PLENA KIRASO",		#full plate
+	"BATALA ARMAĴO",	#battle suit
+	"ETA ŜILDO",		#small shield
+	"GRANDA ŜILDO",		#large shield
+	"MAGIA ŜILDO",		#magic shield
+	"BATALA KASKO",		#battle helmet
+	"DILUVO",			#deluge
+	"TONDRO",			#thunder
+	"FAJRO",			#fire
+	"MORTO",			#death
+	"TILT",				#tilt
+	"RINGO DE ELFOJ",	#ring of elf
+	"RUBENA RINGO",		#ruby ring
+	"RINGO DE NANOJ",	#ring of dworf
+	"DEMONA RINGO",		#demon's ring
+	"ŜLOSILO A",		#key a
+	"ŜLOSILO K",		#key k
+	"ŜLOSILO Q",		#key q
+	"ŜLOSILO J",		#key j
+	"ŜLOSILO JO",		#key jo
+	"PIKPIOĈO",			#mattock
+	"MAGIA VERGO",		#rod		(unused)
+	"KRISTALO",			#crystal	(unused)
+	"LAMPO",			#lamp		(unused)
+	"SABLOHORLOĜO",		#hour glass
+	"LIBRO",			#book		(unused)
+	"FLUGANTAJBOTOJ",	#wing boots
+	"RUĜA TRINKAĴO",	#red potion
+	"NIGRA TRINKAĴO",	#black potion (unused)
+	"ELIKSIRO",			#elixir		(unused)
+	"KOLIERO",			#pendant	(unused)
+	"NIGRA ONIKSO",		#black onix	(unused)
+	"FAJRA KRISTALO"	#fire crystal (unused)
+]
+
+special_chars_items = {
+	'Ĉ': b'C',
+	'Ĝ': b'G',
+	'Ĥ': b'H',
+	'Ĵ': b'J',
+	'Ŝ': b'S',
+	'Ŭ': b'U'
+}
+
+hat_chars = ['Ĉ', 'Ĝ', 'Ĥ', 'Ĵ', 'Ŝ']
+
+breve_chars = ['Ŭ']
+
+def flatten_list_of_bytes(list):
+	return bytes([int(byte) for byte_list in list for byte in byte_list])
+
+def remove_accents(text):
+	return flatten_list_of_bytes([special_chars_items[a] if a in special_chars_items else bytes([ord(a)]) for a in text])
+
+def get_accent(char):
+	if char in hat_chars:
+		return b'\x01'
+	elif char in breve_chars:
+		return b'\x02'
+	else:
+		return b'\x00'
+
+def isolate_accents(text):
+	return flatten_list_of_bytes([get_accent(a) for a in text])
+
+def parse_item_names(names):
+	text = [remove_accents(name) for name in names]
+	accents = [isolate_accents(name) for name in names]
+	return text, accents
+	
+def create_memory_from_text(text):
+	length = len(text)
+	return bytes([length]) + text + bytes(15 - length)
+	
+def create_memory_from_item_names(names):
+	text, accents = parse_item_names(names)
+	text_mem = flatten_list_of_bytes([create_memory_from_text(t) for t in text])
+	accents_mem = flatten_list_of_bytes([create_memory_from_text(a) for a in accents])
+	return text_mem, accents_mem
+
 with open("Faxanadu (USA).nes", "rb") as file:
 	ines_header = file.read(0x10)
 	rom = bytearray(file.read())
@@ -84,18 +171,21 @@ with open("faxanadu_eo_font.chr", "rb") as file:
 	font = file.read()
 	patch(rom, font, 0x34000)
 	
+with open("faxanadu_eo_font_menus.chr", "rb") as file:
+	font = file.read()
+	patch(rom, font, 0x28200)
+	
 with open("text_eo.txt", "r", encoding = "utf-8") as file:
 	raw_text = file.read();
 	new_text = parse_text(raw_text)
-	#print(new_text)
 	patch(rom, new_text, 0x34300)
+	
+text_mem, accents_mem = create_memory_from_item_names(item_names)
+patch(rom, text_mem, 0x31b3d)
+patch(rom, accents_mem, 0x32d90)
 	
 #Increase text speed
 patch(rom, b"\x01", 0x3f49f)
-
-#==========================================#==========================================#
-#  TODO! Split text to not override stuff in the rom after the original script
-#==========================================#==========================================#
 	
 with open("Faxanadu (EO).nes", "wb") as file:
 	file.write(ines_header)
